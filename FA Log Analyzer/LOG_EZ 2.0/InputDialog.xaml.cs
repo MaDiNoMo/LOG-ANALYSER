c7b5aefe2d4e80e97dee11a99aa8d1d024f38f83
ARJUN TR <arjuntryt@gmail.com>
2026-06-16 12:15:18 +0530

    Changed directory path

---
+++ FA Log Analyzer/LOG_EZ 2.0/InputDialog.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+
+namespace LOG_EZ
+{
+    public partial class InputDialog : Window
+    {
+        public string Answer => txtInput.Text;
+
+        public InputDialog(string prompt, string defaultAnswer = "")
+        {
+            InitializeComponent();
+            lblPrompt.Text = prompt;
+            txtInput.Text = defaultAnswer;
+            txtInput.Focus();
+            txtInput.SelectAll();
+        }
+
+        private void BtnOk_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+        }
+
+        private void BtnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+        }
+    }
+}
