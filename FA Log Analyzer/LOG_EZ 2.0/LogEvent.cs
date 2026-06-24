89c57ad69211c1c0d5d2ce947b35d826ed0e8e19
ARJUN TR <arjuntryt@gmail.com>
2026-06-24 12:05:29 +0530

    Committing new files

---
+++ FA Log Analyzer/LOG_EZ 2.0/LogEvent.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LOG_EZ
+{
+    public class LogEvent
+    {
+        public string LogDate { get; set; } = "";
+        public string LogTime { get; set; } = "";
+        public string Timestamp { get; set; } = "";
+        public string SdrMessage { get; set; } = "";
+        public string Protocol { get; set; } = "S6F11"; // S6F11, S3F17, S14F9, etc.
+        public string WaitBit { get; set; } = "";
+
+        // S6F11 Fields
+        public string DataID { get; set; } = "-";
+        public string CEID { get; set; } = "-";
+        public string ReportID { get; set; } = "-";
+
+        // S3F17 Fields
+        public string CarrierAction { get; set; } = "-";
+        public string CarrierID { get; set; } = "-";
+        public string PortID { get; set; } = "-";
+        public string AttrID { get; set; } = "-";
+
+        // S14F9 Fields
+        public string ObjSpec { get; set; } = "-";
+        public string AttrID_S14F9 { get; set; } = "-";
+        public string AttrData { get; set; } = "-";
+
+        public int LineNumber { get; set; }
+
+        public string GetSignature() => Protocol switch
+        {
+            "S3F17" => $"[Carrier:{CarrierID} | Port:{PortID} | Attr:{AttrID}]",
+            "S14F9" => $"[ObjSpec:{ObjSpec} | AttrID:{AttrID_S14F9}]",
+            _ => $"[Data:{DataID} | CEID:{CEID} | Rep:{ReportID}]"
+        };
+    }
+}
---
+++ FA Log Analyzer/LOG_EZ 2.0/SdrEngine.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using System.Runtime.InteropServices;
+
+namespace LOG_EZ
+{
+    public static class SdrEngine
+    {
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        public delegate void ResultCallback(bool isExpectedTree, string timestamp, string dataID, string ceid, string reportID, string colorHex);
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        public delegate void SummaryCallback(int perfectMatches, int totalExpected, int extraMessages);
+
+        [DllImport(@"C:\Users\ArJuN\source\repos\SdrParserEngine\x64\Release\SdrParserEngine.dll", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+        public static extern void CompareLogSequence(string filePath, string startTime, string endTime, string expectedListStr, ResultCallback itemCallback, SummaryCallback summaryCallback);
+    }
+}
